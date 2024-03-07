8486868ebea809dab3afbc949811968e9239a720
SaiprasadAS <162570741+SaiprasadAS@users.noreply.github.com>
2024-03-07 16:20:13 +0530

    Create Pattren_prg.py

---
+++ Pattren_prg.py
@@ -0,0 +1,13 @@
+def print_pattren(rows):
+    for i in range(1, rows+1):
+        for j in range(1, i+1):
+            print('* ', end="")
+        print()
+
+    for k in range(rows+1, 0, -1):
+        for l in range(1, k-1):
+            print(' *', end="")
+        print()
+
+num = 4
+print_pattren(num)
